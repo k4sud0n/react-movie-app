e538e34dbbe9b2e8c9d8f3c489d00bb3ff25197a
Sihyun Kim <legitcode267@gmail.com>
2019-12-31 20:24:25 -0700

    Styling the movies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
 
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
 
   getMovies = async () => {
-    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    const { 
+      data: { 
+        data: { movies } 
+      } 
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
   }
 
   componentDidMount() {
@@ -17,13 +25,33 @@ class App extends React.Component {
 
   render() {
 
-    const { isLoading } = this.state;
-
+    const { isLoading, movies } = this.state;
 
     return (
-      <div>
-        { isLoading ? "Loading..." : "We are ready" }    
-      </div>
+      <section class="container">
+        {isLoading 
+          ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+          )
+          : ( 
+          <div class="movies">
+            {
+             movies.map(movie => (
+              <Movie 
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary}
+                poster={movie.medium_cover_image} 
+              />
+             ))
+            }
+          </div>
+          )
+        }    
+      </section>
     )
   }
 }
---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Movie.css';
+
+function Movies({ id, year, title, summary, poster }) {
+    return (
+        <div class="movie">
+            <img src={poster} alt={title} title={title} />
+            <div class="movie__data">
+                <h3 class="movie__title">{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Movies;
