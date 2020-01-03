136c111b9c985117e78887b4337e91da75b3fa33
Sihyun Kim <legitcode267@gmail.com>
2020-01-02 20:30:59 -0700

    Updates

---
+++ precache-manifest.05de2bb0793464f44d03d693cb70d1b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97b7d633788836365099b5e9a957b48",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "fe4d8716a175575294e6",
+    "url": "/react-movie-app/static/css/main.59828ec2.chunk.css"
+  },
+  {
+    "revision": "390eb3f66990af41ca27",
+    "url": "/react-movie-app/static/js/2.eef1f62f.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/react-movie-app/static/js/2.eef1f62f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe4d8716a175575294e6",
+    "url": "/react-movie-app/static/js/main.ff61cbde.chunk.js"
+  },
+  {
+    "revision": "f1107e1a66ef49474eb1",
+    "url": "/react-movie-app/static/js/runtime-main.7fa604e9.js"
+  }
+]);
