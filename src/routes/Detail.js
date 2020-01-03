40360104bf01b99a90d3ad9141b0e337bb587ecf
Sihyun Kim <legitcode267@gmail.com>
2020-01-02 20:25:49 -0700

    Editing detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
-function Detail() {
-    return (
-        <div></div>
-    )
+class Detail extends React.Component {
+
+    componentDidMount() {
+        const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push('/');
+        } 
+    }
+
+    render() {
+
+        const { location } = this.props;
+
+        if (location.state) {
+            return (
+                <div className="about__container">
+                    <span>{location.state.title}</span>
+                    <br /><br />
+                    <img src={location.state.poster} alt={location.state.title} />
+                    <br /><br />
+                    <p>{location.state.summary}</p>
+                </div>
+            )
+        } else {
+            return null;
+        }
+    }
 }
 
 export default Detail;
